6c73e482bc3d50a558756b5bb78250f26af50793
Andrii <andreyzp87@gmail.com>
2024-01-21 19:00:12 +0200

    Add error handling to file operations

--- task1.py
+++ task1.py
@@ -17,12 +17,28 @@ def copy_files(source: Path, output: Path):
         else:
             ext = path.suffix.replace('.', '')
             out = output / ext
-            out.mkdir(parents=True, exist_ok=True)
-            shutil.copy(path, out / path.name)
+
+            try:
+                out.mkdir(parents=True, exist_ok=True)
+                shutil.copy(path, out / path.name)
+            except OSError:
+                print(f'Cannot copy "{path}" to "{out}"')
 
 
 def main():
     args = get_arguments()
+
+    if not args.source.exists():
+        print(f'Source path "{args.source}" does not exist')
+        exit(1)
+
+    if not args.dist.exists():
+        try:
+            args.dist.mkdir(parents=True, exist_ok=True)
+        except OSError:
+            print(f'Cannot create destination path "{args.dist}"')
+            exit(1)
+
     copy_files(args.source, args.dist)
 
 
