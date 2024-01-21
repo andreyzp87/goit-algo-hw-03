6cac94b4664d3a0c19fd1eeae44bcae6284ebb4b
Andrii <andreyzp87@gmail.com>
2024-01-21 16:51:21 +0200

    fix output folder name

--- task1.py
+++ task1.py
@@ -1,14 +1,3 @@
-# Парсинг аргументів. Скрипт має приймати два аргументи командного рядка: шлях до вихідної директорії та шлях до директорії призначення (за замовчуванням, якщо тека призначення не була передана, вона повинна бути з назвою dist).
-# Рекурсивне читання директорій:
-# Має бути написана функція, яка приймає шлях до директорії як аргумент.
-# Функція має перебирати всі елементи у директорії.
-# Якщо елемент є директорією, функція повинна викликати саму себе рекурсивно для цієї директорії.
-# Якщо елемент є файлом, він має бути доступним для копіювання.
-# Копіювання файлів:
-# Для кожного типу файлів має бути створений новий шлях у вихідній директорії, використовуючи розширення файлу для назви піддиректорії.
-# Файл з відповідним типом має бути скопійований у відповідну піддиректорію.
-# Обробка винятків. Код має правильно обробляти винятки, наприклад, помилки доступу до файлів або директорій.
-
 import argparse
 import shutil
 from pathlib import Path
@@ -17,7 +6,7 @@ from pathlib import Path
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--source', type=Path, default=Path('source'))
-    parser.add_argument('-o', '--output', type=Path, default=Path('output'))
+    parser.add_argument('-d', '--dist', type=Path, default=Path('dist'))
     return parser.parse_args()
 
 
@@ -34,7 +23,7 @@ def copy_files(source: Path, output: Path):
 
 def main():
     args = get_arguments()
-    copy_files(args.source, args.output)
+    copy_files(args.source, args.dist)
 
 
 if __name__ == '__main__':
